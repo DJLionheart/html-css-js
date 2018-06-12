89f72b2fe40663ba47dc16de1c2114d70c6d8c84
DJLionheart <aundario@gmail.com>
2018-06-12 11:44:18 -0600

    style updates

---
+++ main.js
@@ -0,0 +1,9 @@
+
+var input = document.getElementById("updates")
+
+function subscribe() {
+    // console.log('Input: ', input.value);
+    alert(`${input.value} has been subscribed!`);
+    input.value = '';
+
+}
